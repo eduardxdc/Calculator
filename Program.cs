cb11c44ebe0d0d2bc4e64f6340f19fa69764c8c5
eduardxdc <eduardxdc@proton.me>
2025-02-25 16:00:39 -0300

    feat: creating the calculator sum function

--- Program.cs
+++ Program.cs
@@ -7,17 +7,22 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Sum();
+        }
+
+        static void Sum()
+        {
             Console.Clear();
 
             Console.WriteLine("Enter the first value:");
-            float firstValue = float.Parse(Console.ReadLine());
+            float sumFirstValue = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter the second value:");
-            float secondValue = float.Parse(Console.ReadLine());   
+            float sumSecondValue = float.Parse(Console.ReadLine());   
               
             Console.WriteLine("==============================");
 
-            float sumResult = firstValue + secondValue; 
+            float sumResult = sumFirstValue + sumSecondValue; 
 
             Console.WriteLine($"Result of the sum is: {sumResult}");
         }
