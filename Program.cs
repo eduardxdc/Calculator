3d4fa229ef23a3dccd42a6f8c8d92f44aa980fee
eduardxdc <eduardxdc@proton.me>
2025-02-25 15:38:05 -0300

    feat: requesting values ​​from the user and presenting them in the console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics;
 
 namespace Calculator
 {
@@ -6,7 +7,15 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            Console.WriteLine("Enter the first value: ");
+            float firstValue = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value: ");
+            float secondValue = float.Parse(Console.ReadLine());       
+
+            Console.WriteLine(firstValue + " " + secondValue);
         }
     }
 }
