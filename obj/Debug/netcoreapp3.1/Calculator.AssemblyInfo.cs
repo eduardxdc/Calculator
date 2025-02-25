af70e5687924885e4c7df924fc0f752b0605e1f7
eduardxdc <eduardxdc@proton.me>
2025-02-25 15:20:37 -0300

    refactor: configuring the project to meet netcoreapp3.1 specifications

--- obj/Debug/netcoreapp3.1/Calculator.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b7583c6f85da7dad8d9f0c8dc33d67c03c93c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
