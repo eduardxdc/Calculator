81cc6e3a670af278c143c6d0871062eafcabeb5a
eduardxdc <eduardxdc@proton.me>
2025-02-25 17:00:19 -0300

    feat: calling mathematical functions in the menu

--- Program.cs
+++ Program.cs
@@ -16,16 +16,24 @@ namespace Calculator
             Console.WriteLine("Welcome! Select below which mathematical operation you want to perform:");
             Console.WriteLine("");
 
-            Console.WriteLine("1- Sum");
-            Console.WriteLine("2- Subtraction");
-            Console.WriteLine("3- Division");
-            Console.WriteLine("4- Multiplication");
+            Console.WriteLine("1 => Sum");
+            Console.WriteLine("2 => Subtraction");
+            Console.WriteLine("3 => Division");
+            Console.WriteLine("4 => Multiplication");
 
             Console.WriteLine("");
             Console.Write("Enter your choice: ");
 
-            short menuResul = short.Parse(Console.ReadLine());
-            Console.WriteLine($"Good! You chose the option {menuResul}");
+            short menuResult = short.Parse(Console.ReadLine());
+
+            switch (menuResult)
+            {
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Division(); break;
+                case 4: Multiplication(); break;
+                default: Menu(); break;
+            }
 
             Console.ReadKey();
         }
@@ -34,72 +42,76 @@ namespace Calculator
         {
             Console.Clear();
 
-            Console.WriteLine("Enter the first value:");
+            Console.WriteLine("[Sum] Enter the first value:");
             float sumFirstValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second value:");
+            Console.WriteLine("[Sum] Enter the second value:");
             float sumSecondValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("==============================");
+            Console.WriteLine("=============================");
 
             float sumResult = sumFirstValue + sumSecondValue;
             Console.WriteLine($"Result of the sum is: {sumResult}");
 
             Console.ReadKey();
+            Menu();
         }
 
         static void Subtraction()
         {
             Console.Clear();
 
-            Console.WriteLine("Enter the first value:");
+            Console.WriteLine("[Subtraction] Enter the first value:");
             float subFirstValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second value:");
+            Console.WriteLine("[Subtraction] Enter the second value:");
             float subSecondValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("==============================");
+            Console.WriteLine("=====================================");
 
             float subResult = subFirstValue - subSecondValue;
             Console.WriteLine($"Result of the subtraction is: {subResult}");
 
             Console.ReadKey();
+            Menu();
         }
 
         static void Division()
         {
             Console.Clear();
 
-            Console.WriteLine("Enter the first value:");
+            Console.WriteLine("[Division] Enter the first value:");
             float divFirstValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second value:");
+            Console.WriteLine("[Division] Enter the second value:");
             float divSecondValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("==============================");
+            Console.WriteLine("==================================");
 
             float divResult = divFirstValue / divSecondValue;
             Console.WriteLine($"Result of the division is: {divResult}");
 
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplication()
         {
             Console.Clear();
 
-            Console.WriteLine("Enter a first value:");
+            Console.WriteLine("[Multiplication] Enter a first value:");
             float multiFirstValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter a second value:");
+            Console.WriteLine("[Multiplication] Enter a second value:");
             float multiSecondValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("==============================");
+            Console.WriteLine("======================================");
 
             float multiResult = multiFirstValue * multiSecondValue;
             Console.WriteLine($"Result of the multiplication is: {multiResult}");
 
             Console.ReadKey();
+            Menu();
         }
     }
 
